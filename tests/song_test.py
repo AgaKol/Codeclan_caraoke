608ff573263b35f970742569fcbadee85f56222a
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-10-28 19:29:50 +0100

    song - basic tests added

--- tests/song_test.py
+++ tests/song_test.py
@@ -3,4 +3,11 @@ from classes.song import Song
 
 
 class SongTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.song = Song("Like a Prayer", "Madonna")
+
+    def test_song_has_name(self):
+        self.assertEqual("Like a Prayer", self.song.title)
+
+    def test_song_has_singer(self):
+        self.assertEqual("Madonna", self.song.singer)
